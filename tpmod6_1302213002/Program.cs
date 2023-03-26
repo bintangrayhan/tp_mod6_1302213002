102f904d3d5fadf1ce28301845e2c72226cc1e69
bintangrayhan <127047367+bintangrayhan@users.noreply.github.com>
2023-03-26 21:05:18 +0700

    “implementasi design by contract

--- tpmod6_1302213002/Program.cs
+++ tpmod6_1302213002/Program.cs
@@ -6,9 +6,7 @@ class Program
     {
         SayaTubeVideo video = new SayaTubeVideo("Tutorial Design by Contract - [Bintang rayhan wijaya_PRAKTIKAN]");
 
-        //test prekondisi
-        //video = new SayaTubeVideo(null);
-        //video = new SayaTubeVideo("Judul video dengan panjang lebih dari 100 karakter akan menghasilkan ContractException");
+        
 
         for (int i = 0; i < 1000000000; i += 10000000)
         {
