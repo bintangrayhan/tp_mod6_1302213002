eff0e895db9631e84389c60934a7c92c7aec63bb
bintangrayhan <127047367+bintangrayhan@users.noreply.github.com>
2023-03-26 19:48:47 +0700

    Add project files.

---
+++ tpmod6_1302213002/Program.cs
@@ -0,0 +1,19 @@
+﻿using tpmod6_1302213002;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design by Contract - [Bintang rayhan wijaya_PRAKTIKAN]");
+
+        //test prekondisi
+        //video = new SayaTubeVideo(null);
+        //video = new SayaTubeVideo("Judul video dengan panjang lebih dari 100 karakter akan menghasilkan ContractException");
+
+        for (int i = 0; i < 1000000000; i += 10000000)
+        {
+            video.IncreasePlayCount(10000000);
+        }
+        video.PrintVideoDetails();
+    }
+}
